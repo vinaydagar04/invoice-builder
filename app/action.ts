103490afdeaf6dd4e2ea82f2528c7121bbce3681
Vinay Kumar <vinaydagar514@gmail.com>
2025-03-11 12:27:13 +0530

    Add invoice creation functionality with validation and update schema

--- app/action.ts
+++ app/action.ts
@@ -2,9 +2,10 @@
 
 import { requireUser } from "./utils/hooks";
 import { parseWithZod } from "@conform-to/zod";
-import { onboardingSchema } from "./utils/zodSchemas";
+import { invoiceSchema, onboardingSchema } from "./utils/zodSchemas";
 import { prisma } from "./utils/db";
-import { redirect } from "next/navigation";
+import { redirect } from "next/navigation"; 
+
 
 export async function onboardUser(prevState: any, formData: FormData) {
   const session = await requireUser();
@@ -30,6 +31,37 @@ export async function onboardUser(prevState: any, formData: FormData) {
   return redirect("/dashboard");
 }
 
-export async function createInvoice() {
+export async function createInvoice(formData: FormData) {
   const session = await requireUser();
+
+  const submission = parseWithZod(formData, {
+    schema: invoiceSchema,
+  });
+
+  if (submission.status !== "success") {
+    return submission.reply();
+  }
+
+  const data = await prisma.invoice.create({
+    data: {
+      clientAddress: submission.value.clientAddress,
+      clientEmail: submission.value.clientEmail,
+      clientName: submission.value.clientName,
+      currency: submission.value.currency,
+      date: submission.value.date,
+      dueDate: submission.value.dueDate,
+      fromAddress: submission.value.fromAddress,
+      fromEmail: submission.value.fromEmail,
+      fromName: submission.value.fromName,
+      invoiceItemDescription: submission.value.invoiceItemDescription,
+      invoiceItemQuantity: submission.value.invoiceItemQuantity,
+      invoiceItemRate: submission.value.invoiceItemRate,
+      invoiceName: submission.value.invoiceName,
+      invoiceNumber: submission.value.invoiceNumber,
+      status: submission.value.status,
+      total: submission.value.total,
+      note: submission.value.note,
+      userId: session.user?.id,
+    },
+  });
 }
