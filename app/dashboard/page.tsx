5b9779379852d12653088eaf36b8710e15825891
Vinay Kumar <vinaydagar514@gmail.com>
2025-03-14 01:15:51 +0530

    Add DashboardBlocks component to display invoice statistics and refactor Dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,19 +1,12 @@
+import { DashboardBlocks } from "../components/DashboardBlocks";
 import { signOut } from "../utils/auth";
 import { requireUser } from "../utils/hooks";
 
 export default async function Dashboard() {
   const session = await requireUser();
   return (
-    <div>
-      <h1>Hello from dashboard route</h1>
-      <form
-        action={async () => {
-          "use server";
-          await signOut();
-        }}
-      >
-        <button type="submit">Sign Out</button>
-      </form>
-    </div>
+    <>
+      <DashboardBlocks />
+    </>
   );
 }
